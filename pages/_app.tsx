3a1fd9a770f0de1238c0a3df59fcad6d1ad2cb57
kotaro1574 <shake1574@gmail.com>
2022-01-30 09:03:49 +0900

    add: chakra_ui導入

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,13 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Header } from '../components/Header'
+import { ChakraProvider } from '@chakra-ui/react'
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
-  <>
+  <ChakraProvider>
     <Header />
     <Component {...pageProps} />
-  </>
+  </ChakraProvider>
 )
 
 export default MyApp
